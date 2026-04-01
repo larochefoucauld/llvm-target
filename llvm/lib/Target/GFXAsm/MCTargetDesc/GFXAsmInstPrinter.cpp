922c9aefc04819df76ddf6d103f006c08d87ad73
larochefoucauld <aqlwiansalen@gmail.com>
2026-04-01 22:19:12 +0300

    [GFXAsm]: Add InstPrinter implementation

---
+++ llvm/lib/Target/GFXAsm/MCTargetDesc/GFXAsmInstPrinter.cpp
@@ -0,0 +1,47 @@
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/MC/MCExpr.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCSymbol.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "GFXAsmInfo.h"
+#include "GFXAsmInstPrinter.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "asm-printer"
+
+#include "GFXAsmGenAsmWriter.inc"
+
+void GFXAsmInstPrinter::printRegName(raw_ostream &O, MCRegister Reg) {
+  O << getRegisterName(Reg);
+}
+
+void GFXAsmInstPrinter::printInst(const MCInst *MI, uint64_t Address,
+                                  StringRef Annot, const MCSubtargetInfo &STI,
+                                  raw_ostream &O) {
+  printInstruction(MI, Address, O);
+  printAnnotation(O, Annot);
+}
+
+void GFXAsmInstPrinter::printOperand(const MCInst *MI, int OpNo,
+                                     raw_ostream &O) {
+  const MCOperand &MO = MI->getOperand(OpNo);
+
+  if (MO.isReg()) {
+    printRegName(O, MO.getReg());
+    return;
+  }
+
+  if (MO.isImm()) {
+    O << MO.getImm();
+    return;
+  }
+
+  assert(MO.isExpr() && "Unknown operand kind in printOperand");
+  MO.getExpr()->print(O, &MAI);
+}
--- llvm/lib/Target/GFXAsm/MCTargetDesc/GFXAsmInstPrinter.h
+++ llvm/lib/Target/GFXAsm/MCTargetDesc/GFXAsmInstPrinter.h
@@ -2,7 +2,6 @@
 #define LLVM_LIB_TARGET_GFXASM_INSTPRINTER_GFXASMINSTPRINTER_H
 
 #include "llvm/MC/MCInstPrinter.h"
-#include "llvm/MC/MCRegister.h"
 #include "llvm/Support/ErrorHandling.h"
 
 namespace llvm {
@@ -14,15 +13,17 @@ public:
       : MCInstPrinter(MAI, MII, MRI) {}
 
   std::pair<const char *, uint64_t>
-  getMnemonic(const MCInst &MI) const override {
-    llvm_unreachable("GFXAsmInstPrinter::getMnemonic");
-  }
+  getMnemonic(const MCInst &MI) const override;
+  void printInstruction(const MCInst *MI, uint64_t Address, raw_ostream &O);
+  static const char *getRegisterName(MCRegister Reg);
+  static void printOperand(const MCOperand &MO, raw_ostream &O);
 
   // Override MCInstPrinter.
-  void printRegName(raw_ostream &O, MCRegister Reg) override {}
+  void printRegName(raw_ostream &O, MCRegister Reg) override;
   void printInst(const MCInst *MI, uint64_t Address, StringRef Annot,
-                 const MCSubtargetInfo &STI, raw_ostream &O) override {}
+                 const MCSubtargetInfo &STI, raw_ostream &O) override;
+  void printOperand(const MCInst *MI, int OpNo, raw_ostream &OS);
 };
-} // end namespace llvm
+} // namespace llvm
 
 #endif // LLVM_LIB_TARGET_GFXASM_INSTPRINTER_GFXASMINSTPRINTER_H
