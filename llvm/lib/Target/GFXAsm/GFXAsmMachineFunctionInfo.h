3ffd84dba46dff4b3daaeea5a779c62c0f9f3ed0
larochefoucauld <aqlwiansalen@gmail.com>
2026-04-01 21:50:23 +0300

    [GFXAsm] Implement calling convention and function lowering

---
+++ llvm/lib/Target/GFXAsm/GFXAsmMachineFunctionInfo.h
@@ -0,0 +1,57 @@
+//===----------------------------------------------------------------------===//
+//
+// This file declares GFXAsm-specific per-machine-function information.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_GFXASM_GFXASMMACHINEFUNCTIONINFO_H
+#define LLVM_LIB_TARGET_GFXASM_GFXASMMACHINEFUNCTIONINFO_H
+
+#include "llvm/CodeGen/MachineFunction.h"
+
+namespace llvm {
+
+/// GFXAsmFunctionInfo - This class is derived from MachineFunction private
+/// GFXAsm target-specific information for each MachineFunction.
+class GFXAsmFunctionInfo : public MachineFunctionInfo {
+  virtual void anchor();
+
+  bool ReturnStackOffsetSet = false;
+  unsigned ReturnStackOffset = -1U;
+
+  /// FrameIndex for start of varargs area
+  int VarArgsFrameIndex = 0;
+  /// Size of the save area used for varargs
+  int VarArgsSaveSize = 0;
+  /// Size of stack frame to save callee saved registers
+  unsigned CalleeSavedStackSize = 0;
+
+public:
+  GFXAsmFunctionInfo() {}
+  explicit GFXAsmFunctionInfo(MachineFunction &MF) {}
+  ~GFXAsmFunctionInfo() {}
+
+  void setVarArgsFrameIndex(int Off) { VarArgsFrameIndex = Off; }
+  int getVarArgsFrameIndex() const { return VarArgsFrameIndex; }
+
+  void setVarArgsSaveSize(int Size) { VarArgsSaveSize = Size; }
+  int getVarArgsSaveSize() const { return VarArgsSaveSize; }
+
+  unsigned getCalleeSavedStackSize() const { return CalleeSavedStackSize; }
+  void setCalleeSavedStackSize(unsigned Size) { CalleeSavedStackSize = Size; }
+
+  void setReturnStackOffset(unsigned Off) {
+    assert(!ReturnStackOffsetSet && "Return stack offset set twice");
+    ReturnStackOffset = Off;
+    ReturnStackOffsetSet = true;
+  }
+
+  unsigned getReturnStackOffset() const {
+    assert(ReturnStackOffsetSet && "Return stack offset not set");
+    return ReturnStackOffset;
+  }
+};
+
+} // namespace llvm
+
+#endif // LLVM_LIB_TARGET_GFXASM_GFXASMMACHINEFUNCTIONINFO_H
